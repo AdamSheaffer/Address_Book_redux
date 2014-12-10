201554c43ebe2ebbdcd7e56e17bfe10a1d7a1df0
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2014-12-09 18:41:29 -0600

    get, post, and delete requests to firebase

--- js/main.js
+++ js/main.js
@@ -4,12 +4,8 @@
   angular.module('contactApp', [])
     .controller('contactController', function($http){
       var vm = this;
-      // vm.contacts = [
-      //   {name: 'Adam Sheaffer', email: 'adam.e.sheaffer@gmail.com', phone: '123-456-7890', group: 'Friend', img: "https://avatars1.githubusercontent.com/u/9039241?v=3&s=460"},
-      //   {name: 'John Doe', email: 'jDoe@gmail.com', phone: '123-456-7890', group: 'Family', img: "http://www.johndoe.pro/img/John_Doe.jpg"}
-      // ];
 
-      $http.get('https://myaddressbook.firebaseio.com/.json')
+      $http.get('https://myaddressbook.firebaseio.com/contacts.json')
         .success(function(data) {
           vm.contacts = data;
         })
@@ -18,13 +14,28 @@
         })
 
       vm.addContact = function() {
-        vm.contacts.push(vm.newContact);
-        vm.newContact = {};
+        $http.post('https://myaddressbook.firebaseio.com/contacts.json', vm.newContact)
+          .success(function(data){
+            vm.contacts[data.name] = vm.newContact;
+          })
+          .error(function(err){
+            console.log(err);
+          });
+        // vm.contacts.push(vm.newContact);
+        //vm.newContact = {};
       };
 
-      vm.removeContact = function(contact) {
-        var index = vm.contacts.indexOf(contact);
-        vm.contacts.splice(index, 1);
+      vm.removeContact = function(contactId) {
+        var url = 'https://myaddressbook.firebaseio.com/contacts/' + contactId + '.json';
+        $http.delete(url)
+          .success(function(){
+            delete vm.contacts[contactId];
+          })
+          .error(function(err){
+            console.log(err);
+          });
+        // var index = vm.contacts.indexOf(contact);
+        // vm.contacts.splice(index, 1);
       };
 
       vm.groups = {Friends: "Friend", Family: "Family", Work: "Work"}
