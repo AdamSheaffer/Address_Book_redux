9e8432a08d861d734ec155bc1f1002c40d864cff
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2014-12-09 20:49:08 -0600

    added details page for contacts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 ;(function(){
   'use strict';
 
-  angular.module('contactApp', [])
+  angular.module('contactApp', ['ngRoute'])
+    .config(function($routeProvider){
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/table.html',
+        controller: 'contactController',
+        controllerAs: 'contactCtrl'
+      })
+      .when('/new', {
+        templateUrl: 'views/form.html',
+        controller: 'contactController',
+        controllerAs: 'contactCtrl'
+      })
+      .when('/:id', {
+        templateUrl: 'views/details.html',
+        controller: 'detailsController',
+        controllerAs: 'detailsCtrl'
+      })
+      .otherwise({redirectTo: '/'});
+      })
+
+    .controller('detailsController', function($http, $routeParams) {
+      debugger
+      var vm = this;
+      var id = $routeParams.id;
+      var url = 'https://myaddressbook.firebaseio.com/contacts/' + id + '.json';
+      $http.get(url)
+        .success(function(data){
+          vm.contact = data;
+        })
+        .error(function(err){
+          console.log(err);
+        })
+    })
+
     .controller('contactController', function($http){
       var vm = this;
 
@@ -17,12 +51,11 @@
         $http.post('https://myaddressbook.firebaseio.com/contacts.json', vm.newContact)
           .success(function(data){
             vm.contacts[data.name] = vm.newContact;
+            vm.newContact = {};
           })
           .error(function(err){
             console.log(err);
           });
-        // vm.contacts.push(vm.newContact);
-        //vm.newContact = {};
       };
 
       vm.removeContact = function(contactId) {
